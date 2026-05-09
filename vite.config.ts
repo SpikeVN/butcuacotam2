f6178472a4c468389b6ee1f3fd57e171f9660478
SpikeVN <triphuongvasa@gmail.com>
2026-05-09 17:38:54 +0700

    should be live

--- vite.config.ts
+++ vite.config.ts
@@ -2,35 +2,15 @@ import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 import devtools from 'solid-devtools/vite';
-// import UnpluginInjectPreload from 'unplugin-inject-preload/vite';
-
-// import preloadAssets from 'vite-plugin-preload-assets';
 
 export default defineConfig({
   plugins: [
     devtools(),
     solidPlugin(),
     tailwindcss(),
-    // preloadAssets({
-    //   fontsToPreload: [
-    //     { href: "/assets/fonts/bricolage-grotesque-v9-latin_vietnamese-regular.woff2" },
-    //     { href: "/assets/fonts/bricolage-grotesque-v9-latin_vietnamese-600.woff2" },
-    //     { href: "/assets/fonts/cormorant-garamond-v21-latin_vietnamese-regular.woff2" }
-    //   ],
-    // })
-    // UnpluginInjectPreload({
-    //   files: [
-    //     {
-    //       entryMatch: /\/assets\/fonts\/cormorant-garamond-v21-latin_vietnamese-(regular|600)\.woff2/
-    //     },
-    //     {
-    //       entryMatch: /\/assets\/fonts\/cormorant-garamond-v21-latin_vietnamese-regular\.woff2/
-    //     }
-    //   ]
-    // })
   ],
   // Temporarily before moving on to a custom domain
-  base: "/butcuacotam2/",
+  base: "/",
   server: {
     port: 3000,
   },
